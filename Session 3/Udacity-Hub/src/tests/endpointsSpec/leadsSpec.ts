4c519bef3a99494f9ff098d7474bbcd9d26c93a1
Hossam Abubakr <sam.baker990@gmail.com>
2022-03-20 17:39:07 +0200

    =-Session 3: Added three test cases for the leads endpoint

--- Session 3/Udacity-Hub/src/tests/endpointsSpec/leadsSpec.ts
+++ Session 3/Udacity-Hub/src/tests/endpointsSpec/leadsSpec.ts
@@ -3,4 +3,16 @@ import app from '../../index';
 
 const request = supertest(app);
 
-describe('Testing the leads endpoint', () => {});
+describe('Testing the leads endpoint', () => {
+  it('Using the endpoint without providing the name parameter returns 400', async () => {
+    await request.get('/leads').expect(400);
+  });
+
+  it('Using the endpoint with a non-existent lead returns 404', async () => {
+    await request.get('/leads?name=Ali').expect(404);
+  });
+
+  it('Using the endpoint with a valid lead returns 200', async () => {
+    await request.get('/leads?name=Hossam').expect(200);
+  });
+});
