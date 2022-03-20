d5829ec79029fde2c2d9acfbd89f2b1324af7cfb
Hossam Abubakr <sam.baker990@gmail.com>
2022-03-20 03:33:02 +0200

    =-Session 3: Added express and starter server code

--- Session 3/Udacity-Hub/src/index.ts
+++ Session 3/Udacity-Hub/src/index.ts
@@ -1,1 +1,12 @@
-let userName: string = "Hossam Abubakr";
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Udacity Hub API');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
