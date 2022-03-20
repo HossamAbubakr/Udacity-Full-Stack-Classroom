620aadb644d6edc71213cb83c1aec26fe1e92a43
Hossam Abubakr <sam.baker990@gmail.com>
2022-03-20 03:56:31 +0200

    =-Session 3: Implemented the basic functionality Added the basic functionality to the main index.ts

--- Session 3/Udacity-Hub/src/index.ts
+++ Session 3/Udacity-Hub/src/index.ts
@@ -1,10 +1,34 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Udacity Hub API');
+});
+
+// Sample data which we will later get from a real database
+const leads: string[] = ['Hossam', 'Zanaty', 'Amr', 'Ziyad'];
+
+app.get('/leads', (req, res) => {
+  const name = req.query.name as string;
+  const imgLocation = path.resolve('./') + `/assets/${name}.jpg`;
+  const lead = leads.includes(name);
+  // If the name query wasn't provided return and end function
+  if (name === undefined) {
+    return res
+      .status(400)
+      .send('Bad request, query parameter (name) is required.');
+  }
+  // If the name doesn't exist in the array return and end function
+  if (lead === false) {
+    res
+      .status(404)
+      .send('Resource not found, this session lead does not exist!');
+  }
+  // Otherwise return the lead's avatar
+  res.sendFile(imgLocation);
 });
 
 app.listen(port, () => {
