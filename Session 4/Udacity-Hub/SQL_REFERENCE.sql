103b2366b0af9f0aa4746924a4547f3fd2ff857c
Hossam Abubakr <sam.baker990@gmail.com>
2022-03-29 19:26:31 +0200

    =-Session 4: Added the basic config steps for PSQL

--- Session 4/Udacity-Hub/SQL_REFERENCE.sql
+++ Session 4/Udacity-Hub/SQL_REFERENCE.sql
@@ -2,3 +2,12 @@
 
     /* Download & Install PostgreSQL https://www.postgresql.org/download */
     /* Add PSQL to PATH https://sqlbackupandftp.com/blog/setting-windows-path-for-postgres-tools */
+
+    /* Login to PSQL as postgres user */
+    psql -U postgres
+
+    /* Create a new database */
+    CREATE DATABASE udacity_hub;
+
+    /* Connect to the new database */
+    \c udacity_hub;
