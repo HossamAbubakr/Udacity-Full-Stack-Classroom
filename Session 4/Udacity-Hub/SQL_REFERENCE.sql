4c1b3432712145a56798aabfb756652588c08921
Hossam Abubakr <sam.baker990@gmail.com>
2022-03-29 19:25:31 +0200

    =-Session 4: Created a SQL Reference file

---
+++ Session 4/Udacity-Hub/SQL_REFERENCE.sql
@@ -0,0 +1,4 @@
+/* >>>>>>>>> PSQL Initial configuration */
+
+    /* Download & Install PostgreSQL https://www.postgresql.org/download */
+    /* Add PSQL to PATH https://sqlbackupandftp.com/blog/setting-windows-path-for-postgres-tools */
