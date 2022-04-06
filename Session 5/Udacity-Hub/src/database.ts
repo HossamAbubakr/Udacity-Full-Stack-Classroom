0eb1d6f380c7642cc6fe9c8bb35ec7fa1e0fd496
Hossam Abubakr <sam.baker990@gmail.com>
2022-04-07 00:03:48 +0200

    =-Session 5: imported dotenv and used it in db.ts 1.Loaded the environment variables from the .env file 2.Destructured the process.env object 3.Connected to the database using the environment variables

--- Session 5/Udacity-Hub/src/database.ts
+++ Session 5/Udacity-Hub/src/database.ts
@@ -1,10 +1,15 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load the .env file's content into the process.env object
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env; // Destructuring the process.env object
 
 const client = new Pool({
-  host: '127.0.0.1',
-  user: 'postgres',
-  password: 'postgres',
-  database: 'udacity_hub',
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
 
 export default client;
