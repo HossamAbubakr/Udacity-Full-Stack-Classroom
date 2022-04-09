899a79ff048c3f7b6a4f367653491155b9114166
Hossam Abubakr <sam.baker990@gmail.com>
2022-04-09 19:46:56 +0200

    =-Session 6: Implement show method - Session handler

--- Session 6/Udacity-Hub/src/handlers/sessions.ts
+++ Session 6/Udacity-Hub/src/handlers/sessions.ts
@@ -11,8 +11,19 @@ const index = async (req: Request, res: Response) => {
   }
 };
 
+const show = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    const session = await sessions.show(id);
+    res.send(session);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 const sessions_routes = (app: express.Application) => {
   app.get('/sessions', index);
+  app.get('/sessions/:id', show);
 };
 
 export default sessions_routes;
