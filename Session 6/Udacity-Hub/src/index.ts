d94f946d94171a1c0858261c10feb587df50b0e9
Hossam Abubakr <sam.baker990@gmail.com>
2022-04-09 18:32:34 +0200

    =-Session 6: Updated index.ts to use our new routes

--- Session 6/Udacity-Hub/src/index.ts
+++ Session 6/Udacity-Hub/src/index.ts
@@ -1,16 +1,14 @@
 import express from 'express';
-import client from './database';
+import leads_routes from './handlers/session_leads';
 
 const app = express();
 const port = 3000;
 
-app.get('/', async (req, res) => {
-  const connection = await client.connect(); // Create a new connection to the database
-  const query = 'SELECT * FROM students'; // Create a query to select all students
-  const results = await connection.query(query); // Execute the query
-  connection.release(); // Release the connection
-  res.send(results.rows); // Send the results
+app.get('/', async (_req, res) => {
+  res.send('Welcome to the Udacity Hub Api');
 });
+
+leads_routes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
