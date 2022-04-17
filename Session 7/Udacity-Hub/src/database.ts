cb51b1aa194850d39c5858eaf4e748ccca15d9f2
Hossam Abubakr <sam.baker990@gmail.com>
2022-04-17 19:09:30 +0200

    =-Session 7: Included testing environment settings 1.Added a db-migrate testing envuironment settings 2.Added a pg testing environment settings 3.Added two new environment variables to the .env file (ENV/TEST_DB_NAME) 4.Added documentation for the environment variables example file 5.Updated the testing script to reflect the new changes

--- Session 7/Udacity-Hub/src/database.ts
+++ Session 7/Udacity-Hub/src/database.ts
@@ -3,13 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load the .env file's content into the process.env object
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env; // Destructuring the process.env object
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, TEST_DB_NAME, ENV } =
+  process.env; // Destructuring the process.env object
 
 const client = new Pool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
-  database: DB_NAME,
+  database: ENV === 'dev' ? DB_NAME : TEST_DB_NAME,
 });
 
 export default client;
