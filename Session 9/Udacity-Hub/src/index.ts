30739094a5e292e937c1823fd843fbb426616bb0
Hossam Abubakr <sam.baker990@gmail.com>
2022-05-12 05:56:00 +0200

    =-Session 9: Included a switch for the port so that EB can dynamically set the application port

--- Session 9/Udacity-Hub/src/index.ts
+++ Session 9/Udacity-Hub/src/index.ts
@@ -4,7 +4,7 @@ import students_routes from './handlers/students';
 import sessions_routes from './handlers/sessions';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
